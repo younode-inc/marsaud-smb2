a0420a0c28dc77cba30fceff79df6f4f9c4d5bcc
Julien Fontanet <julien.fontanet@isonoe.net>
2018-12-03 11:52:32 +0100

    chore: use Standard config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,16 @@
 module.exports = {
-  env: {
-    node: true,
-  },
-  extends: ['eslint:recommended', 'plugin:node/recommended'],
+  extends: ['standard', 'prettier'],
   globals: {
     Promise: true,
   },
   parserOptions: {
     ecmaVersion: 5,
   },
-  plugins: ['node'],
   rules: {
-    indent: 'off',
-    'linebreak-style': ['error', 'unix'],
-    quotes: [
-      'error',
-      'single',
-      {
-        avoidEscape: true,
-      },
-    ],
-    semi: ['error', 'always'],
+    // detect incorrect import/require
+    'node/no-extraneous-import': 'error',
+    'node/no-extraneous-require': 'error',
+    'node/no-missing-require': 'error',
+    'node/no-missing-import': 'error',
   },
 };
